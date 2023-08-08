afc638111de0421ee4bd81556f9341e3429e30bd
Burak <baruchkahraman@gmail.com>
2023-08-09 01:55:33 +0300

    tutorial and level revisit

--- Assets/BaruchBase/Scripts/Tutorial System/TutorialManager.cs
+++ Assets/BaruchBase/Scripts/Tutorial System/TutorialManager.cs
@@ -1,3 +1,6 @@
+using Baruch.Core;
+using Baruch.Extension;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Baruch
@@ -5,12 +8,44 @@ namespace Baruch
     [DisallowMultipleComponent]
     public class TutorialManager : Singleton<TutorialManager>, IInit
     {
-        [Save]public static byte TutorialStage;
+        [Save] public static byte TutorialStage;
+
+        [SerializeField] GameObject _tutorial;
+        [SerializeField] Transform _hand;
 
         public void Init()
         {
+            Game.OnStateChange += Game_OnStateChange;
+            DOVirtual.Float(0f, 1f, 2f, SetHandPosition).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
 
         }
 
+        private void Game_OnStateChange(GameState obj)
+        {
+            if (obj == GameState.Idle)
+            {
+                Invoke(nameof(Play), 1f);
+            }
+            else if (obj == GameState.Play)
+            {
+                Stop();
+            }
+        }
+
+
+        private void Play()
+        {
+            _tutorial.SetActive(true);
+        }
+        private void Stop()
+        {
+            CancelInvoke();
+            _tutorial.SetActive(false);
+        }
+        private void SetHandPosition(float x)
+        {
+            _hand.position = Vector3.Slerp(Vector3.up + Vector3.right * 0.01f, Vector3.down, x.Remap(0f, 1f, 0.9f, 0.5f)) * 30f;
+
+        }
     }
 }
